9637d84d350054189310aef5a65325c2d3f53e81
denis <denismaximov98@gmail.com>
2020-09-26 11:50:47 +0300

    feat: auth controller done

--- chat-auth/src/controllers/AuthController.ts
+++ chat-auth/src/controllers/AuthController.ts
@@ -4,7 +4,8 @@ import { Request, Response } from "express";
 import "express-async-errors";
 import passwordHash from "password-hash";
 import * as jwt from "jsonwebtoken";
-import jwt_decode from "jwt-decode";
+import { deleteSession, setSession } from '../redis';
+
 import { BadRequestError } from "chat-errors-package";
 export default class AuthController {
   static signin = async (req: Request, res: Response) => {
@@ -22,6 +23,14 @@ export default class AuthController {
       throw new BadRequestError("Invalid credentials");
     }
 
+    user.loggedIn = true;
+
+    try {
+      await userRepository.save(user)
+    } catch (e) {
+      throw new BadRequestError("Could not sign in")
+    }
+
     const jwt_data = {
       user_id: user.id,
     };
@@ -29,10 +38,44 @@ export default class AuthController {
       expiresIn: "30s",
     });
 
+    setSession(user.id, token);
+
     res.status(200).json({user, token})
   };
-  static signup = async (req: Request, res: Response) => {};
-  static signout = async (req: Request, res: Response) => {};
+
+  static signup = async (req: Request, res: Response) => {
+    const userRepository = getRepository(User);
+    let user = new User();
+    user = req.body;
+    user.password = passwordHash.generate(req.body.password);
+
+    try {
+      await userRepository.save(user)
+    } catch (e) {
+      throw new BadRequestError("Could not create a user")
+    }
+
+    res.status(200).json(user)
+  };
+
+  static signout = async (req: Request, res: Response) => {
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne(req.body.id);
+
+    user.loggedIn = false;
+
+    try {
+      await userRepository.save(user);
+    } catch (e) {
+      throw new BadRequestError("Could not logout")
+    }
+
+    deleteSession(req.body.id);
+
+    res.status(200).json(user)
+    
+  };
+
   static test = async (req: Request, res: Response) => {
     const userRepository = getRepository(User);
     const users = await userRepository.find();
--- chat-auth/src/entity/User.ts
+++ chat-auth/src/entity/User.ts
@@ -15,4 +15,7 @@ export class User extends BaseEntity{
     @Column({nullable: true})
     email: string;
 
+    @Column({nullable: true})
+    loggedIn: boolean;
+
 }
